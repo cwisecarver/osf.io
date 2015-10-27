ee0496ece8e93c5065d663445ea1ac89a9ee5a3e
Chris Wisecarver <cwisecarver@cos.io>
2015-10-27 11:48:11 -0400

    Flake8
    [#OSF-4982]

--- website/frozen_trees/listeners.py
+++ website/frozen_trees/listeners.py
@@ -2,7 +2,7 @@ from framework.guid.model import signals as guid_signals
 
 @guid_signals.guid_stored_object_saved.connect
 def log_object_saved(sender, guid_stored_object):
-    print   '#####################################\n'\
-            'Signal "{}" caught for {}: {}\n'\
-            '#####################################'\
+    print '#####################################\n'\
+          'Signal "{}" caught for {}: {}\n'\
+          '#####################################'\
         .format(sender, guid_stored_object._name, guid_stored_object._id)
