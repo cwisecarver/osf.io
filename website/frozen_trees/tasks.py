84408af6fe0df57fc83237b09462c7c48bec9af1
Chris Wisecarver <cwisecarver@cos.io>
2015-10-28 14:17:03 -0400

    Add schema and tasks initial code
    [#OSF-4982]

--- website/frozen_trees/tasks.py
+++ website/frozen_trees/tasks.py
@@ -4,5 +4,10 @@ from celery.utils.log import get_task_logger
 
 from framework.tasks import app as celery_app
 from framework.tasks.utils import logged
+from website.archiver.tasks import create_app_context
 from website.frozen_trees import signals as frozen_tree_signals
 
+class FrozenTreeTask(celery.Task):
+    abstract = True
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
