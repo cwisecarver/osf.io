9c08097f7e3341a0919dd753aebf6305ab00bd25
Chris Wisecarver <cwisecarver@cos.io>
2016-03-07 16:26:07 -0500

    Adding pattern matching for lists

--- api/caching/listeners.py
+++ api/caching/listeners.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from api.caching.tasks import ban_url
+from api.caching.tasks import ban_object_instances_from_varnish
 from framework.tasks.postcommit_handlers import enqueue_postcommit_task
 from modularodm import signals
 
@@ -8,8 +8,13 @@ from modularodm import signals
 @signals.save.connect
 def log_object_saved(sender, instance, fields_changed, cached_data):
     abs_url = None
+    url_pattern_of_type = []
     if hasattr(instance, 'absolute_api_v2_url'):
         abs_url = instance.absolute_api_v2_url
 
+    if hasattr(instance,'_name'):
+        url_pattern_of_type.append('/v2/{}s/$'.format(instance._name))
+        url_pattern_of_type.append('/v2/.*{}s/$'.format(instance._name))
+
     if abs_url is not None:
-        enqueue_postcommit_task(partial(ban_url, abs_url))
+        enqueue_postcommit_task(partial(ban_object_instances_from_varnish, abs_url, url_pattern_of_type))
--- api/caching/tasks.py
+++ api/caching/tasks.py
@@ -11,9 +11,28 @@ def get_varnish_servers():
     #  TODO: this should get the varnish servers from HAProxy or a setting
     return settings.VARNISH_SERVERS
 
-def ban_url(url):
-    timeout = 0.5  # 500ms timeout for bans
-    if settings.ENABLE_VARNISH:
+def ban_url_from_varnish(url, host, timeout, ):
+    try:
+        response = requests.request('BAN', url, timeout=timeout, headers=dict(
+            Host=host
+        ))
+    except Exception as ex:
+        logger.error('Banning {} failed: {}'.format(
+            url,
+            ex.message
+        ))
+    else:
+        if not response.ok:
+            logger.error('Banning {} failed: {}'.format(
+                url,
+                response.text
+            ))
+
+def ban_object_instances_from_varnish(url, url_patterns=None):
+    timeout = 0.3  # 500ms timeout for bans
+    if url_patterns is None:
+        url_patterns = []
+    if settings.ENABLE_VARNISH: 
         parsed_url = urlparse.urlparse(url)
 
         for host in get_varnish_servers():
@@ -23,18 +42,14 @@ def ban_url(url):
                 netloc=varnish_parsed_url.netloc,
                 path=parsed_url.path
             )
-            try:
-                response = requests.request('BAN', url_to_ban, timeout=timeout, headers=dict(
-                    Host=parsed_url.hostname
-                ))
-            except Exception as ex:
-                logger.error('Banning {} failed: {}'.format(
-                    url,
-                    ex.message
-                ))
-            else:
-                if not response.ok:
-                    logger.error('Banning {} failed: {}'.format(
-                        url,
-                        response.text
-                    ))
+            ban_url_from_varnish(url_to_ban, parsed_url.hostname, timeout)
+
+        for pattern in url_patterns:
+            for host in get_varnish_servers():
+                varnish_parsed_url = urlparse.urlparse(host)
+                url_to_ban = '{scheme}://{netloc}{pattern}'.format(
+                    scheme=varnish_parsed_url.scheme,
+                    netloc=varnish_parsed_url.netloc,
+                    pattern=pattern
+                )
+                ban_url_from_varnish(url_to_ban, parsed_url.hostname, timeout)
